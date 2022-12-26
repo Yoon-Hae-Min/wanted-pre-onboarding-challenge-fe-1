5495c97975cca5eba116bcdf17ab0b23daa7944f
Yoon-Hae-Min <yunhatmi@naver.com>
2022-12-26 22:54:30 +0900

    chore: global style 적용 목록 초기화

--- frontend/src/styles/globalStyle.ts
+++ frontend/src/styles/globalStyle.ts
@@ -1,6 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
+    ${reset}
+    *{
+        box-sizing: border-box;
+    }
+    button {
+      border: none;
+      background: none;
+      padding: 0;
+      cursor: pointer;
+    }
     body{
         box-sizing: border-box;
         background-color:${({ theme }) => theme.colors.gray[2]}
