c918e6683bbb606beff20e93e54e7cc2df41119b
Yoon-Hae-Min <yunhatmi@naver.com>
2022-12-29 14:07:27 +0900

    feat: BoardHeader 추가

--- frontend/src/components/Board/Board.styles.tsx
+++ frontend/src/components/Board/Board.styles.tsx
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
-import { BoardFrameProps } from './Board';
+import { BoardProps } from './Board';
 
-export const BoardFrame = styled.div<BoardFrameProps>`
+export const BoardFrame = styled.div<BoardProps>`
   height: ${(props) => props.height};
   width: ${(props) => props.width};
   background-color: ${({ theme }) => theme.colors.white[0]};
   box-shadow: 0rem 0.25rem 1.25rem 0rem ${({ theme }) => theme.colors.gray[1]};
   border-radius: 0.625rem;
+`;
+
+export const BoardHeader = styled.div<BoardProps>`
+  height: ${(props) => props.height};
+  width: 100%;
+  color: ${({ theme }) => theme.colors.white[0]};
+  background-color: ${({ theme }) => theme.colors.blue[3]};
+  border-radius: 0.625rem 0.625rem 0rem 0rem;
 `;
--- frontend/src/components/Board/Board.tsx
+++ frontend/src/components/Board/Board.tsx
@@ -1,21 +1,36 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactNode } from 'react';
 import * as Style from './Board.styles';
 
-export interface BoardFrameProps {
-  height: string;
-  width: string;
+export interface BoardProps {
+  height?: string;
+  width?: string;
+  children: ReactNode | string;
 }
 
-interface BoardProps extends BoardFrameProps {
-  children: ReactElement;
-}
-
-const Board: FC<BoardProps> = ({ height, width, children, ...args }) => {
+const BoardFrame: FC<BoardProps> = ({ height = '62.5rem', width = '62.5rem', children, ...args }) => {
   return (
     <Style.BoardFrame height={height} width={width} {...args}>
       {children}
     </Style.BoardFrame>
   );
+};
+
+const BoardHeader: FC<BoardProps> = ({ height = '62.5rem', children, ...args }) => {
+  return (
+    <Style.BoardHeader height={height} {...args}>
+      {children}
+    </Style.BoardHeader>
+  );
+};
+
+interface BoardType {
+  Frame: FC<BoardProps>;
+  Header: FC<BoardProps>;
+}
+
+const Board: BoardType = {
+  Frame: BoardFrame,
+  Header: BoardHeader,
 };
 
 export default Board;
