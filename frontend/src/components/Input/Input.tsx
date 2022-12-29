92b7b74275536111e3f5dfa6c2c33328a086d26a
Yoon-Hae-Min <yunhatmi@naver.com>
2022-12-29 22:17:35 +0900

    refactor: Input props목록을 컴포넌트 props로 받을수 있도록 변경

--- frontend/src/components/Input/Input.tsx
+++ frontend/src/components/Input/Input.tsx
@@ -1,20 +1,15 @@
-import React, { FC } from 'react';
+import React, { FC, InputHTMLAttributes } from 'react';
 import * as Style from './Input.styles';
 
-export interface InputLayoutProps {
-  width?: string;
-  height?: string;
-}
-
-interface InputProps extends InputLayoutProps {
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string;
   placeholder?: string;
 }
 
 const Input: FC<InputProps> = ({ width = '17.5rem', height, placeholder, errorMessage, ...props }) => {
   return (
-    <Style.InputLayout width={width} height={height} {...props}>
-      <Style.InputField placeholder={placeholder} />
+    <Style.InputLayout width={width} height={height}>
+      <Style.InputField placeholder={placeholder} {...props} />
       <Style.ErrorMessage>{errorMessage}</Style.ErrorMessage>
     </Style.InputLayout>
   );
