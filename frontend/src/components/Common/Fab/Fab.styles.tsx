ab9407aa5df162ee2f620ca16e43cc30256467b3
Yoon-Hae-Min <yunhatmi@naver.com>
2023-01-02 11:25:04 +0900

    refactor: Fab 아이콘 스타일 변경

--- frontend/src/components/Common/Fab/Fab.styles.tsx
+++ frontend/src/components/Common/Fab/Fab.styles.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const Fab = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 2rem;
   height: 2rem;
   background-color: ${({ theme }) => theme.colors.blue[5]};
--- frontend/src/components/Common/Fab/Fab.tsx
+++ frontend/src/components/Common/Fab/Fab.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as Style from './Fab.styles';
-import { ReactComponent as PluseIcon } from '../../assets/svg/plus.svg';
+import { ReactComponent as PluseIcon } from '../../../assets/svg/plus.svg';
 
 const Fab = () => {
   return (
     <Style.Fab>
-      <PluseIcon />
+      <PluseIcon height="1.6rem" width="1.6rem" />
     </Style.Fab>
   );
 };
