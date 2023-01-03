1dba2747e643bf6b432742917363485cd6a3e83c
Yoon-Hae-Min <yunhatmi@naver.com>
2023-01-04 00:46:36 +0900

    refactor: Button컴포넌트에 args 속성 추가

--- frontend/src/components/Common/Button/Button.tsx
+++ frontend/src/components/Common/Button/Button.tsx
@@ -1,16 +1,16 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, ButtonHTMLAttributes } from 'react';
 import * as Style from './Button.styles';
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children?: ReactElement | string;
   height?: string;
   width?: string;
   color?: 'primary' | 'warning';
 }
 
-const Button: FC<ButtonProps> = ({ color, height = '1.875rem', width = '7.5rem', children }) => {
+const Button: FC<ButtonProps> = ({ color, height = '1.875rem', width = '7.5rem', children, ...args }) => {
   return (
-    <Style.Button height={height} width={width} color={color}>
+    <Style.Button height={height} width={width} color={color} {...args}>
       {children}
     </Style.Button>
   );
