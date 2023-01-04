5a907e1073b40addfbe88f1ebc213edce8b393da
Yoon-Hae-Min <yunhatmi@naver.com>
2023-01-04 22:12:11 +0900

    refactor: TextArea 타입 추가

--- frontend/src/hooks/useForm.ts
+++ frontend/src/hooks/useForm.ts
@@ -2,14 +2,18 @@ import { ChangeEvent, useState } from 'react';
 
 const useForm = <T extends object = object>(
   initialState: T
-): [T, (event: ChangeEvent<HTMLInputElement>) => void, (event: ChangeEvent<HTMLInputElement>) => void] => {
+): [
+  T,
+  (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void,
+  (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void
+] => {
   const [state, setState] = useState<T>(initialState);
 
-  const getValue = (event: ChangeEvent<HTMLInputElement>) => {
+  const getValue = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     return event.target.value;
   };
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) =>
+  const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
     setState((pre) => ({
       ...pre,
       [event.target.name]: getValue(event),
